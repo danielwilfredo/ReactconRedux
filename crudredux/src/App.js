3fe119823b7cd5e344126a6d590be5cadde9befc
danielwilfredo <xvwilito11@gmail.com>
2020-12-10 20:19:31 -0600

    cambios del ultimo dia creando el header

--- crudredux/src/App.js
+++ crudredux/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Redux</h1>
+    <Router>
+    
+    <Header/>
+    <div className="container mt-5">
+     
+      <Switch>
+        <Route exact path="/" component={Productos} />
+        <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+        <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+      </Switch>
+    </div>
+    </Router>
+    
+
   );
 }
+/*NOTA: Lo que esta dentro del switch solamente mostrara lo que es de esa pagina
 
+Y todo lo que este fuera del switch mostrara el contenido en todas las paginas como en este caso
+el HEADER
+*/
 export default App;
